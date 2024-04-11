d5cc2512018d57952e62b00b857623194c4795b9
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2024-04-11 16:59:11 +0700

    Cap nhat Trang chu

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HỆ THỐNG BÁN SÁCH</title>
+    <title>HỆ THỐNG THƯƠNG MẠI ĐIỆN TỬ VỀ SÁCH</title>
 </head>
 <body>
    <header>
