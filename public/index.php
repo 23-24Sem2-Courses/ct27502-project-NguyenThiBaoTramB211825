43878ec18ed9fb59d1fceafda5468e9282e726fd
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2024-04-12 08:18:25 +0700

    Cap nhat Trang chu

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HỆ THỐNG THƯƠNG MẠI ĐIỆN TỬ VỀ SÁCH</title>
-</head>
+
+<?php
+    include __DIR__ . '/../src/partials/header.php';
+?>
+
 <body>
    <header>
     
