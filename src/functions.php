5e127a6b96b0d1ff0b9a9ad45b402f29d3e282dd
Trâm <156073826+NguyenThiBaoTramB211825@users.noreply.github.com>
2024-04-15 02:23:32 +0700

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -9,8 +9,4 @@ function redirect(string $location): void
 function html_escape(string|null $text): string
 {
     return htmlspecialchars($text ?? '', ENT_QUOTES, 'UTF-8', false);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e30f6dcfda5df6fd1c43cb213c9404864cd29a49
